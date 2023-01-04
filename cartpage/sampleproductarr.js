74d0b3183f9a75dc09851d91e3fe1657e7dd9a7f
Aahwan <vaahwan@gmail.com>
2023-01-04 18:02:03 +0530

    cart page improved

---
+++ cartpage/sampleproductarr.js
@@ -0,0 +1,282 @@
+let mensData = [ 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg", 
+        name: "MEN'S REEBOK SWIM ARUBA FLIP SLIPPERS ", 
+        price: 699, 
+        strikedoffprice: 999, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg", 
+        name: " MEN'S REEBOK RUNNING AUSTIN SHOES", 
+        price: 945, 
+        strikedoffprice: 2099, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg", 
+        name: "MEN'S REEBOK SWIM AVENGER FLIP LP SLIPPERS", 
+        price: 399, 
+        strikedoffprice: 799, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg", 
+        name: "MEN'S REEBOK RUNNING TEMPO SHOES ", 
+        price: 1620, 
+        strikedoffprice: 3599, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg ", 
+        name: " MEN'S REEBOK RUNNING ASTRO BOOSTER SHOES ", 
+        price: 1440, 
+        strikedoffprice: 3199, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg ", 
+        name: " MEN'S TRAINING WORKOUT READY ELITAGE PANTS ", 
+        price: 1999, 
+        strikedoffprice: 3199, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg ", 
+        name: "MEN'S REEBOK TRAINING  POLO T-SHIRTS TEE  ", 
+        price: 769, 
+        strikedoffprice: 1099, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg", 
+        name: "MEN'S REEBOK TRAINING WORKOUT  TEE  ", 
+        price: 1299, 
+        strikedoffprice: 1440, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg ", 
+        name: "MEN'S REEBOK SUPER SOFT FLIP SLIPPERS", 
+        price: 699, 
+        strikedoffprice: 999, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg ", 
+        name: "MEN'S REEBOK RUNNING RUN SHOES  ", 
+        price: 1620, 
+        strikedoffprice: 3599, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg ", 
+        name: "MEN'S REEBOK RUNNING GENESIS RUNNER SHOES  ", 
+        price: 1620, 
+        strikedoffprice: 3599, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg  ", 
+        name: "MEN'S REEBOK SWIM RUSSLE FLIP SLIPPERS  ", 
+        price: 599, 
+        strikedoffprice: 999, 
+    }, 
+    { 
+        image_url: 
+            " https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg ", 
+        name: " MEN'S REEBOK RUNNING TREND RUNNER SHOES", 
+        price: 1575, 
+        strikedoffprice: 3499, 
+    }, 
+    { 
+        image_url:"https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg  ", 
+        name: "UNISEX REEBOK RUNNING ZIG KINETICA II SHOES  ", 
+        price: 9999, 
+        strikedoffprice: 12000, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg  ", 
+        name: " MEN'S REEBOK TRAINING BAS POLO TEE ", 
+        price: 779, 
+        strikedoffprice: 1299, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg ", 
+        name: "MEN'S REEBOK TRAINING FOUNDATION  PANTS  ", 
+        price: 1019, 
+        strikedoffprice: 1699, 
+    }, 
+    { 
+        image_url: 
+            " https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg ", 
+        name: "MEN'S REEBOK TRAINING FOUNDATION PANTS  ", 
+        price: 1019, 
+        strikedoffprice: 1699, 
+    }, 
+    { 
+        image_url: 
+            " https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg ", 
+        name: " MEN'S REEBOK RUNNING TREND SHOES", 
+        price: 1575, 
+        strikedoffprice: 3499, 
+    }, 
+    { 
+        image_url: 
+            " https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg  ", 
+        name: "  MEN'S REEBOK RUNNING ZTAUR RUN SHOES ", 
+        price: 3499, 
+        strikedoffprice: 4999, 
+    }, 
+    { 
+        image_url: 
+            " https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg   ", 
+        name: "MEN'S REEBOK SWIM ULTRA FLIP SLIPPERS   ", 
+        price: 749, 
+        strikedoffprice: 1499, 
+    }, 
+    { 
+        image_url: 
+            " https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg   ", 
+        name: "  MEN'S REEBOK SWIM FLEX CONNECT SANDALS ", 
+        price: 1149, 
+        strikedoffprice: 2299, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg  ", 
+        name: " MEN'S REEBOK RUNNING GENESIS RUNNER SHOE  ", 
+        price: 1620, 
+        strikedoffprice: 3599, 
+    }, 
+    { 
+        image_url: 
+            "  https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg  ", 
+        name: " MEN'S REEBOK TRAINING RUNNER SHORTS  ", 
+        price: 779, 
+        strikedoffprice: 1299, 
+    }, 
+    { 
+        image_url: 
+            "  https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg  ", 
+        name: " MEN REEBOK CLASSICS TRACKSUIT HOODIE  ", 
+        price: 3499, 
+        strikedoffprice: 4999, 
+    }, 
+    { 
+        image_url: 
+            "  https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg  ", 
+        name: "MEN'S REEBOK CLASSIC TRACKSUIT GOLF HOODIE   ", 
+        price: 3499, 
+        strikedoffprice: 4999, 
+    }, 
+    { 
+        image_url: 
+            " https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg   ", 
+        name: " UNISEX REEBOK TRAINING ACTIVE  BAG ", 
+        price: 1399, 
+        strikedoffprice: 2799, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg  ", 
+        name: " MEN'S REEBOK RUNNING TREND RUNNER SHOES", 
+        price: 1575, 
+        strikedoffprice: 3499, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg ", 
+        name: "UNISEX REEBOK CLASSICS  BASKETBALL SHOES", 
+        price: 3919, 
+        strikedoffprice: 5599, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg ", 
+        name: "MEN'S REEBOK RUNNING RUN FUSION  SHOES", 
+        price: 1620, 
+        strikedoffprice: 3599, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg ", 
+        name: "MEN'S REEBOK RUNNING QUICK DRIFTER SHOES", 
+        price: 1620, 
+        strikedoffprice: 3599, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg ", 
+        name: "MEN'S REEBOK DYNAMIC ZIGZAG SHOES", 
+        price: 7599, 
+        strikedoffprice: 9999, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg ", 
+        name: "MEN'S REEBOK SWIM SUPERSOFT FLIP SLIPPERS", 
+        price: 909, 
+        strikedoffprice: 1299, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg ", 
+        name: "MEN'S REEBOK TRAINING ACTIVE BASIC POLO TEE", 
+        price: 779, 
+        strikedoffprice: 1299, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg ", 
+        name: "MEN'S REEBOK TRAINING ACTIVE MOVE TEE ", 
+        price: 1749, 
+        strikedoffprice: 2499, 
+    }, 
+    { 
+        image_url: 
+            "https://m.media-amazon.com/images/I/71TawoxTk6L._UY500_.jpg ", 
+        name: "MEN'S REEBOK FITNESS TRAINING  SWEATSHIRT", 
+        price: 1079, 
+        strikedoffprice: 1799, 
+    }
+]
+
+
+
+
+showdata(mensData);
+
+
+function showdata(mensData){
+    mensData.map(function(elem){
+        let div = document.createElement("div");
+        div.innerHTML = 
+        `<div id="sub-cart-product">
+        <div id="cart-product-imgdiv"><img id="cart-product-img" src=${elem.image_url} alt="product"></div>
+        <div class="product-detail">
+            <h3 class="product-name">${elem.name}</h3>
+            <h3 class="price">$ ${elem.price}</h3>
+            <div class="price-detail">
+                <p> +  $2.10</p>
+                <img src="https://img.shop.com/Image/resources/images/cashback-icon.svg" alt="dollar sign">
+                <p>Cashback</p>
+            </div>
+            <p>Item#: 190777978227</p>
+            <p>Color: Navy</p>
+            <p>Size: 3X Large</p>
+        </div>
+        <button id="save-for-later">Save for Later</button>
+        <button id="remove-cart">Remove</button>
+        </div>`
+
+        document.querySelector(".cart-product").append(div);
+    })
+}
+
+
+
+        
