5788e132b00128067bb95ac06091b3ec9100564d
VershaKumari264 <108085251+VershaKumari264@users.noreply.github.com>
2023-01-08 12:23:40 +0530

    electronics page

---
+++ electronic.js
@@ -0,0 +1,520 @@
+var health = [
+
+    {
+        img:
+            "https://img.shop.com/Image/240000/243300/243391/products/605278518.jpg?plain&size=750x750",
+        name: "Stealth Radar Shield™",
+        soldBy: "Sold by Stealth Radar Shield™",
+        strikedprice: "80.00",
+        price: "40.00",
+        ratio: "+ up to $3.00 / 8% Cashback",
+        shipping: "Free shipping with $50.00 orders",
+        category:"phone"
+
+    },
+    {
+        img:
+            "https://img.shop.com/Image/280000/283700/283767/products/1948854805.jpg?plain&size=400x400",
+            name: "Apple - iPhone 11 64GB - Purple-Unlocked",
+            soldBy: "Sold by Red Tags Cameras",
+        strikedprice: "35.00",
+        price: "15.00",
+        ratio: "+  $2.40 / 3% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"phone"
+    },
+    {
+        img:
+            "https://img.shop.com/Image/290000/291200/291221/products/1949009100.jpg?plain&size=400x400",
+            name: "Folio Wallet Case for Apple iPhone 14 - Brown",
+            soldBy: "Sold by Sahara Case",
+        strikedprice: "35.00",
+        price: "30.00",
+        ratio: "+ up to $1.22 / 2% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"phone"
+    },
+    {
+        img:
+            "https://img.shop.com/Image/290000/291200/291221/products/1949009110.jpg?plain&size=400x400",
+            name: "PolkaDot Hybrid-Flex Hard Shell Case for Apple iPhone 14 Pro Max - Clear/Black/White",
+            soldBy: "Sold by Sahara Case",
+        strikedprice: "35.00",
+        price: "30.00",
+        ratio: "+ up to $1.22 / 2% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"phone"
+    },
+    {
+        img:
+            "https://img.shop.com/Image/250000/251800/251872/products/1903394698.jpg?plain&size=400x400",
+            name: "Apple MWP22AM-A AirPods Pro with Wireless Charging Case, White",
+            soldBy: " Sold by UnbeatableSale",
+        strikedprice: "45.00",
+        price: "40.00",
+        ratio: "+ up to $1.22 / 2% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"headphone"
+    },
+    {
+        img:
+            "https://img.shop.com/Image/250000/251800/251872/products/1903394696.jpg?plain&size=400x400",
+            name: "Apple MV7N2AM-A AirPods with Charge Case",
+            soldBy: " Sold by UnbeatableSale",
+        strikedprice: "89",
+        price: "20",
+        ratio: "+  $0.63 / 3% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"headphone"
+    },
+    {
+        img:
+            "https://img.shop.com/Image/290000/290400/290460/products/1933855467.jpg?plain&size=400x400",
+        name: "SteelSeries Arctis 7+ Wireless 7.1 Surround Sound Gaming Headset - Black",
+        soldBy: " Sold by GamesnGadgetsPlus",
+        strikedprice: "89",
+        price: "53",
+        ratio: "+  $0.63 / 6% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"headphone"
+    },
+    {
+        img:
+            "https://img.shop.com/Image/290000/291500/291508/products/1949179787.jpg?plain&size=400x400",
+        name: "Beats Studio Buds Wireless Noise Cancelling Earbuds Built in MJ503LL/A - Open Box",
+        soldBy: " Sold by RetailKing",
+        strikedprice: "89",
+        price: "53",
+        ratio: "+  $0.63 / 6% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"headphone"
+    },
+
+    {
+        img:
+            "https://img.shop.com/Image/280000/281900/281909/products/1945416977.jpg?plain&size=400x400",
+            name: "Samsung NP950XEEXA1 15.6 inch Galaxy Book 2 Pro AMOLED Laptop - Intel Core i7 - 32GB/1TB",
+            soldBy: " Sold by Electronic Express",
+        strikedprice: "189",
+        price: "53",
+        ratio: "+  $0.63 / 3% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"laptop"
+    },
+    {
+        img:
+            "https://img.shop.com/Image/290000/291500/291508/products/1951724309.jpg?plain&size=400x400",
+            name: "ASUS ROG STRIX 15.6 1920X1080 FHD I7-9750H 16 512GB GTX 1650 WIN 10 BLACK - Open Box",
+        soldBy: " Sold by RetailKing",
+        strikedprice: "77",
+        price: "33",
+        ratio: "+  $0.63 / 3% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"laptop"
+    },
+    {
+        img:
+            "https://img.shop.com/Image/290000/291500/291508/products/1943937940.jpg?plain&size=400x400",
+            name: "ACER CHROMEBOOK 15 C910-C37P 15.6 FHD CELERON 3215U 4GB 32GB SSD BLACK - Open Box",
+            soldBy: " Sold by  RetailKing",
+        strikedprice: "177",
+        price: "39",
+        ratio: "+  $0.63 / 3% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"laptop"
+    },
+    {
+        img:
+            "https://img.shop.com/Image/250000/251800/251872/products/1947374751.jpg?plain&size=400x400",
+            name: "Asus TeK UX7602ZM-XB96T 16 in. Zenbook Pro 16X OLED Multi-Touch Laptop, Black",
+            soldBy: " Sold by  UnbeatableSale",
+        strikedprice: "177",
+        price: "39",
+        ratio: "+  $0.63 / 3% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"laptop"
+    },
+    {
+        img:
+            "https://img.shop.com/Image/280000/281900/281909/products/1935336852.jpg?plain&size=400x400",
+            name: "Apple MK2K3 10.2 inch iPad (Wi-Fi, 64GB) - Space Gray",
+            soldBy: " Sold by  Electronic Express",
+        strikedprice: "57",
+        price: "55",
+        ratio: "+  $0.63 / 3% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"tablet"
+    },
+    {
+        img:
+            "https://img.shop.com/Image/290000/291500/291508/products/1946692441.jpg?plain&size=400x400",
+            name: "SAMSUNG GALAXY TAB A7 10.4 64GB WIFI GOLD SM-T500NZDTXAR - Open Box",
+            soldBy: " Sold by RetailKing",
+        strikedprice: "57",
+        price: "27",
+        ratio: "+  $0.63 / 3% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"tablet"
+    },
+    {
+        img:
+            "https://img.shop.com/Image/290000/291500/291508/products/1942946296.jpg?plain&size=400x400",
+            name: "Lenovo Tab M8 8 HD Touch Helio A22 2GB 32GB eMMC ZA5G0060US Tablet",
+        soldBy: " Sold by RetailKing",
+        strikedprice: "57",
+        price: "25",
+        ratio: "+  $0.63 / 3% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"tablet"
+    },
+    {
+        img:
+            "https://img.shop.com/Image/290000/291500/291508/products/1957450708.jpg?plain&size=400x400",
+            name: "APPLE IPAD 3RD GEN 9.7 16GB WIFI MD339LL/A - BLACK - Open Box",
+        soldBy: " Sold by  RetailKing",
+        strikedprice: "57",
+        price: "25",
+        ratio: "+  $0.63 / 3% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"tablet"
+    },
+    {
+        img:
+            "https://img.shop.com/Image/280000/283700/283767/products/1925077793.jpg?plain&size=400x400",
+            name: "Sony Playstation 5 Digital Edition",
+        soldBy: "Sold by Red Tags Cameras",
+        strikedprice: "80",
+        price: "40",
+        ratio: "+ up to $3.00 / 8% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"videogame"
+
+    },
+    {
+        img:
+            "https://img.shop.com/Image/240000/249900/249914/products/1919128694.jpg?plain&size=400x400",
+       
+            name: "Microsoft Xbox Series S Gaming Console - Game Pad Supported - Wireless - White - AMD Custom RDNA 2 - 1440p - Linear PCM, Dolby Digital 5.1, DTS 5.1, Dolby TrueHD with Atmos - Ultra HD Blu-ray Player - 512 GB HDD - Gigabit Ethernet - - Octa-core (8 Core)",
+            soldBy: "Sold by Red Tags Cameras",
+        strikedprice: "109",
+        price: "79",
+        ratio: "+  $2.40 / 3% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"videogame"
+    },
+    {
+        img:
+            "https://img.shop.com/Image/240000/249900/249914/products/1953846589.jpg?plain&size=400x400",
+            name: "Sony DualShock 4 Wireless Controller - Wireless - Bluetooth - USB - PlayStation 4 - Midnight Blue",
+        soldBy: "Sold by Tech for Less",
+        strikedprice: "60",
+        price: "45",
+        ratio: "+ up to $1.22 / 2% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"videogame"
+    },
+    {
+        img:
+            "https://img.shop.com/Image/290000/290400/290460/products/1924454872.jpg?plain&size=400x400",
+            name: "Xbox One X Fallout 76 Special Edition 1TB Console Bundle RobotWhite New Open Box",
+        soldBy: "Sold by GamesnGadgetsPlus",
+        strikedprice: "60",
+        price: "45",
+        ratio: "+ up to $1.22 / 2% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"videogame"
+    },
+    {
+        img:
+            "https://img.shop.com/Image/280000/281900/281909/products/1869578781.jpg?plain&size=400x400",
+            name: "TCL 32S335 32 inch 3-Series HD LED Smart Roku TV",
+        soldBy: "Sold by Electronic Express",
+        strikedprice: "60",
+        price: "45",
+        ratio: "+ up to $1.22 / 2% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"television"
+    },
+    {
+        img:
+            "https://img.shop.com/Image/280000/281900/281909/products/1891851485.jpg?plain&size=400x400",
+            name: "Samsung UN65AU8000 65 inch AU8000 Crystal UHD Smart TV",
+        soldBy: "Sold by  Electronic Express",
+        strikedprice: "60",
+        price: "45",
+        ratio: "+ up to $1.22 / 2% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"television"
+    },
+    {
+        img:
+            "https://img.shop.com/Image/280000/281900/281909/products/1840586180.jpg?plain&size=400x400",
+            name: "Samsung UN32M4500 32 inch M4500 HD Smart TV",
+        soldBy: "Sold by Electronic Express",
+        strikedprice: "60",
+        price: "45",
+        ratio: "+ up to $1.22 / 2% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"television"
+    },
+    {
+        img:
+            "https://img.shop.com/Image/280000/281900/281909/products/1849756716.jpg?plain&size=400x400",
+            name: "Supersonic SC2812 12 inch LED Display with Digital TV Tuner",
+        soldBy: "Sold by  Electronic Express",
+        strikedprice: "60",
+        price: "45",
+        ratio: "+ up to $1.22 / 2% Cashback",
+        shipping: "Free shipping with $40.00 orders",
+        category:"television"
+    },
+   
+
+];
+displayfun(health);
+
+document.getElementById("filter").addEventListener("change", changed);
+
+function displayfun(q) {
+   
+    document.querySelector("#product-container").innerHTML = "";
+    q.map(function (elem, i) {
+        var box = document.createElement("div");
+
+        var im = document.createElement("img");
+        im.setAttribute("src", elem.img);
+
+        var na = document.createElement("h3");
+        na.innerText = elem.name;
+
+        var sbp = document.createElement("p");
+
+        var logo = document.createElement("img");
+        logo.setAttribute("src", "https://img.shop.com/Image/resources/images/onecart-icon.svg");
+        // logo.setAttribute("class", "cartlogo"); 
+        logo.style.height = "15px"
+        logo.style.width = "15px";
+
+        var sb = document.createElement("span");
+        sb.innerText = elem.soldBy;
+        sb.setAttribute("class", "sold");
+        sbp.append(logo, sb);
+        sbp.style.marginTop = "-15px";
+
+        var pr = document.createElement("p");
+
+        var sp1 = document.createElement("span");
+        sp1.innerText = "$"+elem.strikedprice;
+        sp1.style.fontSize = ".75rem";
+        sp1.style.textDecoration = "line-through";
+        var sp2 = document.createElement("span");
+        sp2.innerText = "$"+elem.price;
+        sp2.setAttribute("class", "oprice");
+        pr.append(sp1, sp2);
+        pr.style.marginTop = "-6px";
+
+        var cbp = document.createElement("p");
+
+        var rato = document.createElement("span");
+        rato.innerText = elem.ratio;
+        rato.style.marginRight = "5px";
+        var logo2 = document.createElement("img");
+        logo2.setAttribute("src", "https://img.shop.com/Image/resources/images/cashback-icon.svg");
+        //logo2.setAttribute("class", "dollar"); 
+        logo2.style.height = "13px";
+        logo2.style.width = "13px";
+        var cb = document.createElement("span");
+        cb.innerText = "Cashback";
+        cbp.append(rato, logo2, cb);
+        cbp.style.fontSize = ".75rem";
+        cbp.style.marginTop = "-8px";
+
+
+        var sp = document.createElement("span");
+        sp.innerText = elem.shipping;
+        sp.style.color = "#007e97";
+        sp.style.fontSize = ".75rem";
+        sp.style.marginBottom = "15px";
+
+        var bdiv = document.createElement("div");
+        bdiv.setAttribute("class", "buttons")
+
+        var sd = document.createElement("div");
+        
+        var anchor=document.createElement("a");
+        anchor.innerText="See Details";
+        sd.append(anchor);
+        anchor.setAttribute("href","details.html");
+        anchor.setAttribute("class","anch");
+        anchor.addEventListener("click",function(){
+            det(elem);
+        });
+        //sd.setAttribute("class", "details"); 
+        var atc = document.createElement("button");
+        atc.innerText = "Add to Cart";
+        atc.addEventListener("click", function () {
+            cart(elem, i);
+        });
+
+        // atc.setAttribute("class", "addtocart"); 
+        bdiv.append(sd, atc)
+
+        box.append(im, na, sbp, pr, cbp, sp, bdiv);
+        document.getElementById("product-container").append(box);
+
+
+    });
+}
+
+function det(elem){
+    var arr=[];
+    arr.push(elem);
+    localStorage.setItem("detail",JSON.stringify(arr));
+//location.replace("details.html");
+}
+
+var cartArr = JSON.parse(localStorage.getItem("cart-list")) || [];
+var count = localStorage.getItem("cartcount") || 0;
+document.querySelector(".cart-count").innerText = count;
+
+function cart(elem, i) {
+    cartArr.push(elem);
+    count++;
+    document.querySelector(".cart-count").innerText = count;
+    localStorage.setItem("cart-list", JSON.stringify(cartArr));
+    localStorage.setItem("cartcount", count);
+}
+
+function changed() {
+    var catt = document.querySelector("#filter").value;
+    if (catt == "") {
+        displayfun(health);
+    } else {
+        var p = health.filter(function (item) {
+            if (item.category == catt) {
+                return item;
+            }
+        });
+        displayfun(p);
+    }
+
+}
+
+document.querySelector("#filtergame").addEventListener("click", filterfun1);
+document.querySelector("#filterhome").addEventListener("click", filterfun2);
+document.querySelector("#filterapparel").addEventListener("click", filterfun3);
+document.querySelector("#filterfootball").addEventListener("click", filterfun4);
+document.querySelector("#filterbath").addEventListener("click", filterfun5);
+document.querySelector("#filterbath1").addEventListener("click", filterfun6);
+
+function filterfun1() {
+    let q =health.filter(function (item) {
+        if (item.category == "phone") {
+            return item;
+        }
+    });
+    displayfun(q);
+}
+
+function filterfun2() {
+    let q = health.filter(function (item) {
+
+
+        if (item.category == "headphone") {
+            return item;
+        }
+    });
+    displayfun(q);
+}
+
+function filterfun3() {
+    let q = health.filter(function (item) {
+        if (item.category == "laptop") {
+            return item;
+        }
+    });
+    displayfun(q);
+}
+function filterfun4() {
+    let q = health.filter(function (item) {
+        if (item.category == "tablet") {
+            return item;
+        }
+    });
+    displayfun(q);
+}
+function filterfun5() {
+    let q = health.filter(function (item) {
+        if (item.category == "videogame") {
+            return item;
+        }
+    });
+    displayfun(q);
+}
+function filterfun6() {
+    let q = health.filter(function (item) {
+        if (item.category == "television") {
+            return item;
+        }
+    });
+    displayfun(q);
+}
+/* <!-- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= --> */
+function onClickMenu() {
+    document.getElementById("menu").classList.toggle("icon");
+    document.getElementById("nav").classList.toggle("change");
+}
+
+/* <!-- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= --> */
+document.querySelector(".logo").addEventListener("click",function(){
+    window.open("project.html");
+});
+
+// search functionality
+
+document.querySelector(".btn1").addEventListener("click",funsearch);
+
+function funsearch(){
+    event.preventDefault();
+    var key=document.querySelector("#search").value;
+    key=key.toUpperCase();
+    if(key.includes("BEAUTY")){
+        window.open("beauty.html","_self");
+        document.querySelector("#search").value="";
+    }else if(key.includes("CLOTH")){
+        window.open("clothe.html","_self");
+        document.querySelector("#search").value="";
+    }else if(key.includes("FOOD")){
+        window.open("food.html","_self");
+        document.querySelector("#search").value="";
+    }else if(key.includes("HEALTH")){
+        window.open("health.html","_self");
+        document.querySelector("#search").value="";
+    }else if(key.includes("HOME")){
+        window.open("home.html","_self");
+        document.querySelector("#search").value="";
+    }else if(key.includes("JWELLERY")){
+        window.open("jwellery.html","_self");
+        document.querySelector("#search").value="";
+    }else if(key.includes("KIDS")){
+        window.open("kids.html","_self");
+        document.querySelector("#search").value="";
+    }else if(key.includes("PET")){
+        window.open("pet.html","_self");
+        document.querySelector("#search").value="";
+    }else if(key.includes("SHOES")){
+        window.open("shoes.html","_self");
+        document.querySelector("#search").value="";
+    }else if(key.includes("SPORTS")){
+        window.open("sports.html","_self");
+        document.querySelector("#search").value="";
+    }
+}
+
+//feedback
+document.getElementById("submitfeedback").addEventListener("click",function(){
+    let val=document.getElementById("feedbacktext").value;
+    if(val.length>0){
+        alert("Thank you for your valuable feedback!!!");
+    }
+});
