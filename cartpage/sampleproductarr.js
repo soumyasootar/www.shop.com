7ba03f357eadfc2fce22a6b0965c9f1e1044a2c9
narayan dutta <narayandutta2208@gmail.com>
2023-01-09 05:09:13 +0530

    Font-size-changes

--- cartpage/sampleproductarr.js
+++ cartpage/sampleproductarr.js
@@ -13,25 +13,25 @@ function showdata(mensData){
         }
         localStorage.setItem("cart-list",JSON.stringify(cart_product_arr)); 
         let div = document.createElement("div");
-        div.setAttribute("class","product-box")
+        div.setAttribute("class","product-box");
         div.innerHTML = 
         `<div id="sub-cart-product">
         <div id="cart-product-imgdiv"><img id="cart-product-img" src=${elem.img} alt="product"></div>
         <div class="product-detail">
             <h3 class="product-name">${elem.name}</h3>
-            <h3 class="price">$ ${elem.price}</h3>
-            <div class="price-detail">
-                <p> +  $2.10</p>
+            <h3 class="price" style="margin-bottom:6px; font-weight:bold">$ ${elem.price}</h3>
+            <div class="price-detail"style="margin-bottom:-16px ; margin-top:-5px;" >
+                <p style="font-size:15px; font-weight:bold"> +  $2.10</p>
                 <img src="https://img.shop.com/Image/resources/images/cashback-icon.svg" alt="dollar sign">
-                <p>Cashback</p>
+                <p style="font-size:15px; font-weight:bold">Cashback</p>
             </div>
-            <p>Item#: 190777978227</p>
-            <p>Sold by: ${elem.soldBy}</p>
-            <p>Category: ${elem.category}</p>
+            <p style="font-size:15px; font-weight:bold">Item: #190777978227</p>
+            <p style="font-size:15px; font-weight:bold">Sold by: ${elem.soldBy}</p>
+            <p style="font-size:15px; font-weight:bold">Category: ${elem.category}</p>
             <div id="quantity-div">
-                <p>Quantity</p>
-                <select name="quantity" id="quantity-count">
-                    <option value="1">1</option>
+                <p style="font-size:15px; font-weight:bold">Quantity</p>
+                <select style="font-size:15px; font-weight:bold; color:#5F9DF7;" name="quantity" id="quantity-count">
+                    <option value="1" >1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
                     <option value="4">4</option>
@@ -46,7 +46,10 @@ function showdata(mensData){
         </div>
         <button id="save-for-later">Save for Later</button>
         <button id="remove-cart" >Remove</button>
-        </div>`
+        </div>
+        
+        
+        `
 
         document.querySelector(".cart-product").append(div);
 
